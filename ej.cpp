773ad6d4d684f5a698be946fe39c0e9b1f49bcd4
Sas2o <samjo1306@hotmail.com>
2019-04-09 20:21:23 -0500

    Ej es el ejercicio de la clase

--- ej.cpp
+++ ej.cpp
@@ -6,20 +6,26 @@ using namespace std;
 void leerDatos();
 void leerArchivo(string nombreArchivo, double* &z);
 double MDMC();
-double suma_x();
+double suma_x(int n);
+double suma_y(int n);
 
 double* x = NULL;
 double* y = NULL;
+int n_xy;
 
 int main() 
 {
     leerDatos();
-    cout << suma_x() << endl; 
+    
+    
     return 0;
 }
 void leerDatos(){
     leerArchivo("valores_x.txt", x);
+    cout << "La suma de los valores en x es: " << suma_x(n_xy) << endl; 
     leerArchivo("valores_y.txt", y);
+    cout << "La suma de los valores en y es: " << suma_y(n_xy) << endl; 
+    cout << "termino la lectura de los archivos" << endl; 
 }
 void leerArchivo(string nombreArchivo, double* &z){
     ifstream infile; 
@@ -33,7 +39,7 @@ void leerArchivo(string nombreArchivo, double* &z){
         n+=1;
         getline(infile, line);
     }
-    
+    n_xy=n;
     infile.close();
     infile.open(nombreArchivo);
     z=new double[n];
@@ -44,11 +50,17 @@ void leerArchivo(string nombreArchivo, double* &z){
     infile.close();
 
 }
-double suma_x(){
+double suma_x(int n){
     double suma=0;
-    for (int i=0; i<sizeof(x);i++){
+    for (int i=0; i<n;i++){
         suma+=x[i];
     }
     return suma;
 }
-
+double suma_y(int n){
+    double suma=0;
+    for (int i=0; i<n;i++){
+        suma+=y[i];
+    }
+    return suma;
+}
